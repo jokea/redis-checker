b4b26a7e44b44f9741f0f4df56f3d608c037af53
Jokea <tfengjun@gmail.com>
2012-07-31 12:00:20 +0800

    [M] redis-check code

---
+++ config.py
@@ -0,0 +1,10 @@
+# time to sleep before checking next key(in seconds)
+interval = 0.001
+
+# server list
+servers = [
+	'127.0.0.1:6379',
+	'127.0.0.1:6380',
+	'127.0.0.1:6381'
+]
+
---
+++ redis-check.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# Check the consistency of redis servers
+
+import redis
+import sys
+import os
+import time
+from config import *
+
+connections = []
+mismatch_set = set()
+
+def connect_all() :
+	for server in servers :
+		tmp = server.partition(':')
+		ip = tmp[0]
+		port = int(tmp[2])
+		r = redis.Redis(ip, port, 0)
+		connections.append(r)
+
+def check(key, type) :
+	first = None
+	for r in connections :
+		if type == 'string' :
+			value = r.get(key)
+			if first == None :
+				first = value
+			else :
+				if value != first :
+					mismatch_set.add(key)
+					return
+		elif type == 'list' :
+			value = r.lrange(key, 0, -1)
+			if first == None :
+				first = value
+			else :
+				if value != first :
+					mismatch_set.add(key)
+					return
+		elif type == 'hash' :
+			value = r.hgetall(key)
+			if first == None :
+				first = value
+			else :
+				if value != first :
+					mismatch_set.add(key)
+					return
+		elif type == 'set' :
+			value = r.smembers(key)
+			if first == None :
+				first = value
+			else :
+				if value != first :
+					mismatch_set.add(key)
+					return
+		elif type == 'zset' :
+			value = r.zrange(key, 0, -1)
+			if first == None :
+				first = value
+			else :
+				if value != first :
+					mismatch_set.add(key)
+					return
+		elif type == 'none' :
+			pass
+
+def main() :
+	connect_all()
+	r = connections[0]
+	if len(sys.argv) == 1 :
+		count = 1
+	else :
+		count = long(sys.argv[1])
+	
+	for i in xrange(0, count) :
+		time.sleep(interval)
+		key = r.randomkey()
+		if key :
+			type = r.type(key)
+			check(key, type)
+
+	print 'Total mismatches', len(mismatch_set)
+	for x in mismatch_set :
+		print x
+
+
+if __name__ == '__main__' :
+	main()
